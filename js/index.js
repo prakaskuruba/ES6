89d860170e852b57be2898ac6e46fe327816411a
prakashkuruba <prakashkuruba111@gmail.com>
2024-04-15 18:22:09 +0530

    sum of n

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+
+const sum=(n)=>{
+    let s=0;
+    for(let i=1;i<=n;i++){
+        s+=i;
+    }    
+    return s;
+}
+let a=100;
+console.log("\n\t sum of "+a+" natural numbers : "+sum(a))
+
+// sum of 100 natural numbers : 5050
